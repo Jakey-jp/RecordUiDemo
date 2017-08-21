4a3b832cfef2447427a0d09ef65c020c62701165
abc784990536 <784990536@qq.com>
2017-08-21 16:53:18 +0800

    Initial commit

---
+++ app/src/main/java/com/jakey/recorduidemo/Constants.java
@@ -0,0 +1,13 @@
+package com.jakey.recorduidemo;
+
+import android.os.Environment;
+
+import java.io.File;
+
+public class Constants {
+
+    public static final class FilePath {
+        public static final String RECORD_TEMP_PATH = Environment.getExternalStorageDirectory() + File.separator + "com.sxbb" + File.separator + "recordTemp" + File.separator;
+    }
+
+}
